9ea9c24e833ed87cc5fff9b94b4752cafdcce1f5
Brad Carman <bradleygcarman@outlook.com>
2024-07-13 16:28:54 +0200

    fixed defaults issue

--- ActiveSuspensionModel.jl/test/init.jl
+++ ActiveSuspensionModel.jl/test/init.jl
@@ -1,8 +1,10 @@
 using Test
 using ActiveSuspensionModel
 using ModelingToolkit
+
 using DifferentialEquations
 
+using ModelingToolkit: t_nounits as t
 using ActiveSuspensionModel: SystemParams, System
 
 @mtkbuild sys = System()
@@ -28,23 +30,11 @@ initialization_eqs = [
     sys.seat_pos.s ~ 1.5
     ]
 
-@named initsys = ModelingToolkit.generate_initializesystem(sys; initialization_eqs, defaults = sys .=> params)
+@named initsys = ModelingToolkit.generate_initializesystem(sys; initialization_eqs, default_p = sys .=> params)
 
 # First let's try without running structural_simplify, we have a balanced set of 104 equations and 104 unknowns...
-iprob = NonlinearProblem(complete(initsys), [], [])
-
-#=
-ERROR: Initial condition underdefined. Some are missing from the variable map.
-Please provide a default (`u0`), initialization equation, or guess
-for the following variables:
-
-Any[seat₊m₊s(t), seat₊m₊v(t), seat₊s₊delta_s(t), car_and_suspension₊m₊s(t), car_and_suspension₊m₊v(t), car_and_suspension₊s₊delta_s(t), wheel₊m₊s(t), wheel₊m₊v(t), wheel₊s₊delta_s(t), seat_pos₊s(t)  …  car_and_suspension₊m₊a(t), seat₊m₊flange₊f(t), seat₊m₊a(t), seat_pos₊sˍtt(t), err₊input1₊uˍtt(t), err₊output₊uˍtt(t), pid₊xˍtt(t), pid₊dxˍt(t), pid₊ddx(t), pid₊dy(t)]
-=#
-
-# Why am I getting a message that guesses are missing, when they clearly are not...
-ModelingToolkit.get_guesses(sys)[sys.seat.m.s] #seat₊m₊initial_position
-ModelingToolkit.get_guesses(sys)[sys.seat.m.v] #-seat₊m₊g*seat₊m₊m
-ModelingToolkit.get_guesses(sys)[sys.seat.s.delta_s] #0
+iprob = NonlinearProblem(complete(initsys), [t=>0], []) #Why do I need to set t to 0?
+isol = solve(iprob) # ConvergenceFailure
 
 
 # OK, so let's try to run structural_simplify, maybe something is needed there to grab the guesses..
